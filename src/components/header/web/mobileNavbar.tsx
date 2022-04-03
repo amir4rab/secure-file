192c2259cbe93b1110e4bcdbc1591efa5e0ef897
Amir Arab <amir4rab@gmail.com>
2022-04-03 11:31:42 +0430

    bug fixes

--- src/components/header/web/mobileNavbar.tsx
+++ src/components/header/web/mobileNavbar.tsx
@@ -35,7 +35,7 @@ function MobileNavbar(props: Omit<NavbarProps, 'children'>) {
                 {...props}
                 hiddenBreakpoint="lg"
                 hidden={false}
-                style={styles}
+                style={{ ...styles, minHeight: 'calc(100vh - 3rem)', top: '3rem' }}
                 width={{ sm: '100%' }}
               >
                 <Center sx={{ height: '100%', margin: 'auto', flexDirection: 'column' }}>
