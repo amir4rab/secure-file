98f864212361c79ee8b5c4e03bbb0a1bc12e92ab
Amir Arab <amir4rab@gmail.com>
2022-04-03 11:32:00 +0430

    added footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,62 @@
+import { ActionIcon, Group, Text, createStyles, Portal, Container } from '@mantine/core'
+import Image from 'next/image'
+import { SiGithub, SiTwitter } from 'react-icons/si';
+import SecureFileLogo from '../../../public/favicon.svg';
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    background: theme.colors.dark[6],
+    padding: `${theme.spacing.xl}px 0`,
+    width: '100%'
+  },
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    [theme.fn.smallerThan('md')]: {
+      flexDirection: 'column'
+    }
+  },
+  group: {
+    [theme.fn.smallerThan('md')]: {
+      ':not(:last-child)': {
+        marginBottom: theme.spacing.md
+      },
+      ':last-child': {
+        display: 'flex',
+        justifyContent: 'flex-end'
+      }
+    }
+  }
+}));
+
+const Footer = () => {
+  const { classes } = useStyles();
+
+  return (
+    <Portal>
+      <footer className={ classes.footer }>
+        <Container className={ classes.wrapper }>
+          <Group className={ classes.group }>
+            <Image src={ SecureFileLogo } alt='logo' width={32} height={32} />
+            <Text component='span'>
+              Secure File
+            </Text>
+            <Text size='xs' component='span' sx={{ opacity: .8 }}>
+              © 2021 secure-file.amir4rab.com All rights reserved.
+            </Text>
+          </Group>
+          <Group className={ classes.group }>
+            <ActionIcon component='a'  href='https://github.com/amir4rab/secure-file' target='_blank' rel='noreferrer'>
+              <SiGithub />
+            </ActionIcon>
+            <ActionIcon component='a'  href='https://github.com/amir4rab/secure-file' target='_blank' rel='noreferrer'>
+              <SiTwitter />
+            </ActionIcon>
+          </Group>
+        </Container>
+      </footer>
+    </Portal>
+  )
+}
+
+export default Footer
