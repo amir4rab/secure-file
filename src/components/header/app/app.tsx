ee2a6a4a3d2e505ed7da40560eb3a38e9441b72c
Amir Arab <amir4rab@gmail.com>
2022-04-05 09:58:58 +0430

    simplified AppHeader

--- src/components/header/app/app.tsx
+++ src/components/header/app/app.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { MediaQuery, Tabs } from '@mantine/core';
+import { MediaQuery, Tabs, createStyles } from '@mantine/core';
 import { IoSettings, IoFolder, IoPaperPlane } from 'react-icons/io5';
 import { useRouter } from 'next/router';
 
@@ -9,10 +9,45 @@ const locationsArray = [
   '/settings'
 ]
 
+const useStyles = createStyles((theme) => ({
+  mobileNavbar: {
+    [theme.fn.largerThan('md')]: {
+      display: 'none'
+    },
+    position: 'fixed', 
+    left: '1rem', 
+    bottom: '1rem', 
+    background: theme.colors.dark[5], 
+    padding: '1rem .5rem', 
+    borderRadius: '1rem', 
+    width: 'calc(100% - 2rem)',
+  },
+  mobileNavbarItem: {
+    width: '30%',
+    borderRadius: '.5rem'
+  },
+  desktopNavbar: {
+    position: 'fixed',
+    top: '2rem',
+    left: '0rem', 
+    bottom: '0rem', 
+    height: 'calc(100% - 2rem)',
+    width: '10rem',
+    padding: '2rem 0',
+    [theme.fn.largerThan('md')]: {
+      left: 'calc(calc(100vw - 960px)/2)'
+    },
+    [theme.fn.smallerThan('md')]: {
+      display: 'none'
+    }
+  }
+})) 
+
 const AppHeader = () => {
   const router = useRouter();
   const [ activeTab, setActiveTab ] = useState(0);
   const [ initialLoad, setInitialLoad ] = useState(true);
+  const { classes } = useStyles();
 
   const changeTab = ( input: number ) => {
     router.push('/app' + locationsArray[input]);
@@ -34,52 +69,32 @@ const AppHeader = () => {
 
   return (
     <>
-      <MediaQuery largerThan='md' styles={{ display: 'none' }}>
-        <Tabs
-          sx={(theme) => ({ 
-            position: 'fixed', 
-            left: '1rem', 
-            bottom: '1rem', 
-            background: theme.colors.dark[5], 
-            padding: '1rem .5rem', 
-            borderRadius: '1rem', 
-            width: 'calc(100% - 2rem)',
-          })} 
-          active={activeTab} 
-          onTabChange={changeTab}
-          variant='pills'
-          tabPadding='md'
-          position='center'
-        >
-          <Tabs.Tab sx={{ width: '30%', borderRadius: '.5rem' }} icon={ <IoFolder/> } label="Folder" />
-          <Tabs.Tab sx={{ width: '30%', borderRadius: '.5rem' }} icon={ <IoPaperPlane/> } label="Connect" />
-          <Tabs.Tab sx={{ width: '30%', borderRadius: '.5rem' }} icon={ <IoSettings/> } label="Settings" />
-        </Tabs>
-      </MediaQuery>
-      <MediaQuery smallerThan='md' styles={{ display: 'none' }}>
-        <Tabs
-          sx={(theme) => ({ 
-            position: 'fixed',
-            top: '2rem',
-            left: '0rem', 
-            bottom: '0rem', 
-            height: 'calc(100% - 2rem)',
-            width: '10rem',
-            padding: '2rem 0',
-            [`@media(min-width:${theme.breakpoints.md}px)`]: {
-              left: 'calc(calc(100vw - 960px)/2)'
-            }
-          })} 
-          active={activeTab} 
-          onTabChange={changeTab}
-          orientation='vertical'
-          tabPadding='lg'
-        >
-          <Tabs.Tab icon={ <IoFolder/> } label="Folder" />
-          <Tabs.Tab icon={ <IoPaperPlane/> } label="Connect" />
-          <Tabs.Tab icon={ <IoSettings/> } label="Settings" />
-        </Tabs>
-      </MediaQuery>
+      {/* Mobile Navbar */}
+      <Tabs
+        className={ classes.mobileNavbar }
+        active={activeTab} 
+        onTabChange={changeTab}
+        variant='pills'
+        tabPadding='md'
+        position='center'
+      >
+        <Tabs.Tab className={ classes.mobileNavbarItem } icon={ <IoFolder/> } label='Folder' />
+        <Tabs.Tab className={ classes.mobileNavbarItem } icon={ <IoPaperPlane/> } label='Connect' />
+        <Tabs.Tab className={ classes.mobileNavbarItem } icon={ <IoSettings/> } label='Settings' />
+      </Tabs>
+
+      {/* Desktop Navbar */}
+      <Tabs
+        className={ classes.desktopNavbar }
+        active={activeTab} 
+        onTabChange={changeTab}
+        orientation='vertical'
+        tabPadding='lg'
+      >
+        <Tabs.Tab icon={ <IoFolder/> } label='Folder' />
+        <Tabs.Tab icon={ <IoPaperPlane/> } label='Connect' />
+        <Tabs.Tab icon={ <IoSettings/> } label='Settings' />
+      </Tabs>
     </>
   )
 }
