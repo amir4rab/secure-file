eb4ef3a907c6f205250ef520ea91b50c2cd74014
Amir Arab <amir4rab@gmail.com>
2022-04-05 09:57:53 +0430

    added New packages & added Dokcer support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa');
 
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  }
+    disable: process.env.NODE_ENV !== 'production',
+  },
+  experimental: {
+    outputStandalone: process.env.IS_DOCKER_BUILD === 'true' ? true : false,
+  },
 })
