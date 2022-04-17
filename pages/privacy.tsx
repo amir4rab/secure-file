10014682ce1282fe68a4d68c151ffb0de6394a3b
Amir Arab <amir4rab@gmail.com>
2022-04-17 08:30:19 +0430

    added privacy draft page

---
+++ pages/privacy.tsx
@@ -0,0 +1,12 @@
+import HeadDetails from '@/components/headDetails'
+import { NextPage } from 'next';
+
+const PrivacyPage: NextPage = () => {
+  return (
+    <>
+      <HeadDetails title='Privacy' />
+    </>
+  )
+}
+
+export default PrivacyPage
