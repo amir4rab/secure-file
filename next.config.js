d878fe44263342b5eb39279bee70d0a9524ada98
Amir Arab <amir4rab@gmail.com>
2022-04-18 10:44:30 +0430

    added next translate

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
+const withPlugins = require('next-compose-plugins');
+const nextTranslate = require('next-translate');
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV !== 'production',
-  },
-  experimental: {
-    outputStandalone: process.env.IS_DOCKER_BUILD === 'true' ? true : false,
-  },
-})
+module.exports = withPlugins(
+  [
+    withPWA,
+    nextTranslate
+  ], 
+  {
+    reactStrictMode: true,
+    pwa: {
+      dest: 'public',
+      disable: process.env.NODE_ENV !== 'production',
+    },
+    experimental: {
+      outputStandalone: process.env.IS_DOCKER_BUILD === 'true' ? true : false,
+    },
+  }
+);
