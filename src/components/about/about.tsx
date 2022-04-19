990a58b443a1fa683f9bfb9a6d36f97d6ee08392
Amir Arab <amir4rab@gmail.com>
2022-04-19 09:14:57 +0430

    added translation for about page

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,25 +1,29 @@
 import { Title, Box, Text, Center, Anchor } from '@mantine/core'
+import useTranslation from 'next-translate/useTranslation'
 import React from 'react'
 import { SiGithub } from 'react-icons/si'
 
 function About() {
+  const { t } = useTranslation('about');
+  const { t: commonT } = useTranslation('common');
+
   return (
     <>
       <Title my='lg' order={1}>
-        About
+        { t('title') }
       </Title>
       <Box>
         <Text>
-          {`Secure file is a free and open-source web-application under GPL-3 license, with hope of being useful for it's users`}
+          { t('subtitle') }
         </Text>
         <Title order={3} mt='lg' mb='md'>
-          Links
+          { commonT('links') }
         </Title>
         <Center sx={{ justifyContent: 'flex-start' }}>
           <Anchor href={ process.env.NEXT_PUBLIC_GITHUB_URL } target='_blank' rel='noreferrer' sx={{ display: 'flex', alignItems: 'center' }}>
             <SiGithub style={{ marginRight: '.5rem', display: 'block' }} />
             <Text>
-              Github
+              { commonT('github') }
             </Text>
           </Anchor>
         </Center>
