84fdca519f8696e989417601140bfbb3eb073b19
Amir Arab <amir4rab@gmail.com>
2022-05-05 09:18:17 +0430

    minor fixes to update modal

--- src/components/updateModal/updateModal.tsx
+++ src/components/updateModal/updateModal.tsx
@@ -1,39 +1,39 @@
 import useIsPwa from '@/hooks/useIsPwa'
 import { Button, Modal, Text, Center } from '@mantine/core';
 import React from 'react'
+
 import DynamicNamespaces from 'next-translate/DynamicNamespaces';
-import useTranslation from 'next-translate/useTranslation';
+import Trans from 'next-translate/Trans';
 
 function UpdateModal() {
   const { newVersionIsAvailable, updateWorker } = useIsPwa();
-  const { t } = useTranslation('update-modal');
-  const { t: commonT } = useTranslation('common');
-
   const closeEvent = () => {
     updateWorker(false);
   }
 
   return (
-    <Modal
-      opened={ newVersionIsAvailable }
-      onClose={ closeEvent }
-      title={ commonT('update') }
-      transition='slide-up'
-      centered
-      closeOnClickOutside={ false }
-    >
-      <Text sx={(theme) => ({ paddingBottom: theme.spacing.xl * 2 })}>
-        { t('prompt') }
-      </Text>
-      <Center sx={{ justifyContent: 'flex-end' }}>
-        <Button onClick={ () => updateWorker(true) } mr='lg'>
-          { commonT('update') }
-        </Button>
-        <Button onClick={ closeEvent } variant='subtle' color='gray'>
-          { commonT('skip') }
-        </Button>
-      </Center>
-    </Modal>
+    <DynamicNamespaces namespaces={[ 'update-modal' ]}>
+      <Modal
+        opened={ newVersionIsAvailable }
+        onClose={ closeEvent }
+        title={ <Trans i18nKey='common:update' /> }
+        transition='slide-up'
+        centered
+        closeOnClickOutside={ false }
+      >
+        <Text sx={(theme) => ({ paddingBottom: theme.spacing.xl * 2 })}>
+          { <Trans i18nKey='update-modal:prompt' /> }
+        </Text>
+        <Center sx={{ justifyContent: 'flex-end' }}>
+          <Button onClick={ () => updateWorker(true) } mr='lg'>
+            { <Trans i18nKey='common:update' /> }
+          </Button>
+          <Button onClick={ closeEvent } variant='subtle' color='gray'>
+            { <Trans i18nKey='common:skip' /> }
+          </Button>
+        </Center>
+      </Modal>
+    </DynamicNamespaces>
   )
 }
 
