95ebd37dadefdb667f66379b6e1392a341e84c67
Amir Arab <amir4rab@gmail.com>
2022-05-07 15:45:22 +0430

    added ConnectFalseQueries

---
+++ src/components/connect-components/connectFalseQueries/connectFalseQueries.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+// mantine components
+import { Box, Text, Title, createStyles } from '@mantine/core';
+
+// components
+import Link from '@/components/link';
+
+// translation
+import useTranslation from 'next-translate/useTranslation';
+import Trans from 'next-translate/Trans';
+
+// styles
+const useStyles = createStyles((theme) => ({
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '60vh'
+  },
+  title: {
+    marginBottom: theme.spacing.xl
+  },
+  text: {
+    flexGrow: 1
+  },
+  errorBox: {
+    background: theme.colors.dark[5],
+    color: theme.colors.gray[3],
+    padding: theme.spacing.xl,
+    borderRadius: theme.radius.md,
+    fontSize: theme.fontSizes.sm
+  }
+}))
+
+interface Props {
+  errorCode: 'falseQueries' | 'noNode'
+}
+const ConnectFalseQueries = ({ errorCode }: Props ) => {
+  const { classes } = useStyles();
+  const { t } = useTranslation('connect-data');
+
+  return (
+    <Box className={ classes.main } >
+      <Title className={ classes.title }  order={3} mb='xl'>
+        { t('wentWrong') }
+      </Title>
+      <Trans
+        i18nKey='connect-data:backToIndex'
+        components={[
+          <Text key={0} className={ classes.text } />,
+          <Link key={1} path='/connect' />
+        ]}
+      />
+      <Box className={ classes.errorBox } >
+        Error Code: { errorCode }
+      </Box>
+    </Box>
+  )
+}
+
+export default ConnectFalseQueries
