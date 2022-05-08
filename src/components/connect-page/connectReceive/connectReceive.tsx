b339de4c3a27c9dcea9fca7aab2984c683088e96
Amir Arab <amir4rab@gmail.com>
2022-05-08 10:35:23 +0430

    added ConnectReceive

---
+++ src/components/connect-page/connectReceive/connectReceive.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+// mantine components
+import { Box, Title, createStyles } from '@mantine/core'
+
+// components
+import ConnectFile from '@/components/connect-components/connectFile';
+
+// types //
+import { ReceiveArray } from '@/types/connect';
+
+// hooks //
+import { useConnectContext } from '@/providers/connectContext';
+import useTranslation from 'next-translate/useTranslation';
+
+// styles
+const useStyles = createStyles((theme) => ({
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  addButton: {
+    fontSize: theme.fontSizes.xs,
+    fontWeight: 400,
+    [ theme.fn.largerThan('md') ] : {
+      fontSize: theme.fontSizes.sm,
+    }
+  }
+}));
+
+interface Props {
+  filesArray: ReceiveArray
+}
+function ConnectReceive({ filesArray }: Props ) {
+  const { classes } = useStyles();
+  const { fileEvent, isBusy } = useConnectContext();
+  const { t } = useTranslation('connect-data');
+
+  return (
+    <>
+      <Box mb='xl' className={ classes.header }>
+        <Title order={3}>{ t('receive') }</Title>
+      </Box>
+      <div>
+        { 
+          filesArray.map(( file ) => (
+            <ConnectFile isDisabled={ isBusy } eventHandler={ fileEvent } key={ file.uuid } type='receive' fileData={ file }/>
+          )) 
+        }
+      </div>
+    </>
+  )
+}
+
+export default ConnectReceive
