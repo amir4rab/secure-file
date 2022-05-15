4a30311af82a782817d8401d446cf60fb42d7204
Amir Arab <amir4rab@gmail.com>
2022-05-15 10:13:53 +0430

    page head fixes

--- pages/app/settings/account.tsx
+++ pages/app/settings/account.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import type { NextPage } from 'next';
-import Head from 'next/head';
+
 import HeadDetails from '@/components/headDetails';
 import { AccountSettings } from '@/components/settings-components';
 
 const AccountSettingsPage: NextPage = () => {
   return (
     <>
-      <Head>
-        <HeadDetails title='Settings' />
-      </Head>
+      <HeadDetails title='Settings' />
       <AccountSettings />
     </>
   )
